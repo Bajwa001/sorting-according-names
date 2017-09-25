bef64a8698a851d8716d85196edba9e8673cf457
Navroop Singh Bajwa <navroop.bajwa@yahoo.com>
2017-09-25 19:55:12 +0530

    Initial Commit

---
+++ sorting according names/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  sorting according names
+//
+//  Created by Navroop Singh Bajwa on 25/09/17.
+//  Copyright © 2017 Bajwa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
